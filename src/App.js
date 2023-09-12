b29d1d5e28e96fadf1e12fc6b1628749d95c439e
mekaala <mekaala@gmail.com>
2023-09-12 17:35:22 -0400

    created as much as possible before api plan ran out

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api/index';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
-function App() {
+const App = () => {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const [coordinates, setCoordinates] = useState({ lat: 33.7488, lng: -84.3877 });
+  const [bounds, setBounds] = useState({});
+
+  const [filteredPlaces, setFilteredPlaces] = useState([])
+  const [places, setPlaces] = useState([]);
+
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+      setCoordinates({ lat: latitude, lng: longitude });
+    })
+  }, [])
+
+  useEffect(() => {
+    const filteredPlaces = places?.filter((place) => place.rating > rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [rating])
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(type, bounds.sw, bounds.ne)
+    .then((data) => {
+      setPlaces(data);
+      setFilteredPlaces([]);
+      setIsLoading(false);
+    })
+  }, [type, coordinates, bounds]);
+
   return (
-    <div className="App">
+    <>
       <CssBaseline/>
       <Header/>
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List/>
+          <List
+          places={places}
+          childClicked={childClicked}
+          isLoading={isLoading}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map/>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
+          />
         </Grid>
       </Grid>
-    </div>
+    </>
   );
 }
 
--- src/components/PlaceDetails/styles.js
+++ src/components/PlaceDetails/styles.js
@@ -4,6 +4,18 @@ export default makeStyles(() => ({
   chip: {
     margin: '5px 5px 5px 0',
   },
+  ratingBox: {
+    display: 'flex', justifyContent: 'space-between',
+  },
+  priceBox: {
+    display: 'flex', justifyContent: 'space-between',
+  },
+  rankBox: {
+    display: 'flex', justifyContent: 'space-between',
+  },
+  awardBox: {
+    display: 'flex', justifyContent: 'space-between',
+  },
   subtitle: {
     display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '10px',
   },
